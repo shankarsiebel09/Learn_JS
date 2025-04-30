a0dfcb928159358e581fd6247a170ee6dee15372
shankarsiebel09 <shankarsiebel09@gmail.com>
2025-04-30 16:27:07 +0530

    Operators

--- First.js
+++ First.js
@@ -1,4 +1,7 @@
-a=10
+a=10.55
 b=20
 c=a+b
 console.log(c)
+
+ 
+console.log(10%3)
---
+++ Operators.js
@@ -0,0 +1,129 @@
+// //1.Arthemmetic operators
+
+// console.log(5+4)
+
+// console.log(10-3)
+
+// console.log(5*3)
+
+// console.log(25/5)
+
+// console.log(25/4)
+
+// console.log(9%3)
+// console.log(100%50)
+// console.log(10%3)
+
+// //another way
+// a=10
+// b=20
+// c=a+b
+// console.log(c)
+
+// a=10
+// b=20
+// c=a-b
+// console.log(c)
+
+// a=10
+// b=20
+// c=a*b
+// console.log(c)
+
+// a=100
+// b=50
+// c=a/b
+// console.log(c)
+
+// a=100
+// b=20
+// c=a%b
+// console.log(c)
+
+//2.Comparision Operators 
+
+// console.log("King"=="King")
+
+// console.log(2==2)
+
+// console.log(2=="2")
+
+// console.log(2===2)
+
+// console.log(2==="2")
+
+//NOTE:
+// "==" --will compare only values but not the datatypes 
+//"===" --will compare values but also the datatype 
+
+// console.log(10>5)
+// console.log(10<5)
+// console.log(5<10)
+// console.log(5>10)
+// console.log(10>=10)
+// console.log(10>10)
+// console.log(10<=10)
+// console.log(10<10)
+
+//3.Logical Operators:
+// console.log((2==2) && (4>3))
+
+// console.log((2==="2") && (4>3))
+
+// console.log((2==="2") || (4>3))
+// console.log((2===2) || (3<4))
+
+// console.log(!(2==2))
+// console.log(!(2===2))
+
+// console.log(!(2=="2"))
+// console.log(!(2==="2"))
+
+// 4.Assignment Operators:
+// x=30
+// console.log(x+=20)
+// x=30
+// console.log(x-=10)
+// x=30
+// console.log(x*=2)
+// x=30
+// console.log(x/=5)
+// x=30
+// console.log(x%=3)
+
+//5. Conditional Operators:
+// x="king"
+// y="Queen"
+// z= 2==2 ? x:y
+// console.log(z)
+
+// x="king"
+// y="Queen"
+// z= 2==="2" ? x:y
+// console.log(z)
+
+// payment=true
+// progress= payment ? "Done":"Inprogress"
+// console.log(progress)
+
+// payment=false
+// progress= payment ? "Done":"Inprogress"
+// console.log(progress)
+
+
+// Q=5+9
+// console.log(Q)
+
+// K="King"+"Queen"
+// console.log(K)
+
+// K="King"+"   "+"Queen"
+// console.log(K)
+
+//6.Coersion
+// adsn=30+"50"
+// console.log(adsn) //-->3050(string)(automatically number got converted ito strong)
+
+// console.log("KING"+100)   //-->KING100(automatically number got converted ito strong)
+
+// console.log(100+200+300+"505"+600+700)
